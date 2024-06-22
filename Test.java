b8304b4ec3df805a59b5e84adb09d25f47f9fc34
debmeet404 <debmeet404@gmail.com>
2024-06-22 11:11:33 +0530

    autoboxing and unboxing

--- Test.java
+++ Test.java
@@ -1,7 +1,17 @@
 public class Test {
     public static void main(String[] args) {
-        // do nothing
-        // more testing!
+        
+        Integer x1 = 1000, x2 = 1000;
+
+        if (x1 == x2) {
+            System.out.println("same");
+        } else {
+            System.out.println("not same");
+        }
+
+        /*
+        Java caches some literals. The above program should logically output not same as they are references, but sometimes it outputs same as well!
+        */
         
     }
 }
